7f512ebe5a5367a599051e97d9c6921d581ae602
mikebraton@163.com <2606557451@qq.com>
2022-04-21 17:39:41 +0800

    扩张援建bug修改

--- src/mount/creep/misson/action.ts
+++ src/mount/creep/misson/action.ts
@@ -274,13 +274,10 @@ export default class CreepMissonActionExtension extends Creep {
             this.arriveTo(new RoomPosition(24,24,missionData.Data.disRoom),10,missionData.Data.shard,missionData.Data.shardData?missionData.Data.shardData:null)
             return
         }
-        if (!this.memory.arrived)
+        if (!this.memory.arrived && Game.flags[`${this.memory.belong}/expand`] && Game.flags[`${this.memory.belong}/expand`].pos.roomName == this.room.name)
         {
-            if (Game.flags[`${this.memory.belong}/expand`] && Game.flags[`${this.memory.belong}/expand`].pos.roomName == this.room.name)
-            {
-                if (!this.pos.isEqualTo(Game.flags[`${this.memory.belong}/expand`])) this.goTo(Game.flags[`${this.memory.belong}/expand`].pos,0)
-                else this.memory.arrived = true
-            }
+            if (!this.pos.isEqualTo(Game.flags[`${this.memory.belong}/expand`])) this.goTo(Game.flags[`${this.memory.belong}/expand`].pos,0)
+            else this.memory.arrived = true
             return
         }
         this.workstate('energy')
