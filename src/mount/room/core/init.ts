ed701df4458f25b861a3d47994ce028e892a0c1a
Morningtea <2528308387@qq.com>
2022-08-13 21:17:23 -0400

    自适应升级优化

--- src/mount/room/core/init.ts
+++ src/mount/room/core/init.ts
@@ -355,6 +355,7 @@ export default class RoomCoreInitExtension extends Room {
             }
             let creep_num = Math.floor(room_energy / 100000);
             creep_num = creep_num > 5 ? 5 : creep_num
+            creep_num = creep_num < 1 ? 1 : creep_num
             if (room_energy < 50000) creep_num = 0;
             if (this.memory.SpawnConfig.upgrade.num != creep_num) { console.log(this.name, 'upgrade动态调整', creep_num); }
             this.memory.SpawnConfig.upgrade.num = creep_num;
@@ -365,6 +366,8 @@ export default class RoomCoreInitExtension extends Room {
                 } else {
                     if (level >= 8) {
                         this.memory.SpawnConfig.upgrade.num = 1;
+                    } else {
+                        this.memory.SpawnConfig.upgrade.num = 2;
                     }
                 }
             }
